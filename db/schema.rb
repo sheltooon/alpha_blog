4c97f1b38cc42cdcacc823762f3b269bc7ef2254
sheltooon <kevinshelton104@gmail.com>
2020-11-07 20:35:08 +0530

    Updated DB_MIgration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_130842) do
+ActiveRecord::Schema.define(version: 2020_11_07_150244) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
